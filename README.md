2225def13fdf4d5e27ab7a68272f0f60e1c0b373
YY <z3388638@gmail.com>
2019-04-26 22:44:30 +0800

    Update meta; add readme and demo

---
+++ README.md
@@ -0,0 +1,12 @@
+# YahooAnswersSpamReport
+Make it easy to report a spam on Yahoo! Answers.
+
+## Install
+1. Install the `Tampermonkey` browser extension ([Google Chrome](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)/[Firefox](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/))
+2. Install `YahooAnswersSpamReport` script  from [OpenUserJS](https://openuserjs.org/scripts/x3388638/YahooAnswersSpamReport)
+
+## Demo
+![](https://github.com/x3388638/YahooAnswersSpamReport/blob/master/demo1.gif)
+  
+![](https://github.com/x3388638/YahooAnswersSpamReport/blob/master/demo2.gif)
+
--- index.js
+++ index.js
@@ -3,13 +3,13 @@
 // @name         YahooAnswersSpamReport
 // @namespace    https://2yc.tw
 // @version      0.1.0
-// @description 
+// @description  Make it easy to report a spam on Yahoo! Answers
 // @author       YY
 // @license      MIT
 // @homepage     https://github.com/x3388638/YahooAnswersSpamReport
 // @updateURL    https://openuserjs.org/meta/x3388638/YahooAnswersSpamReport.meta.js
 // @match        https://*.answers.yahoo.com
-// @match        https://*.answers.yahoo.com/answer
+// @match        https://*.answers.yahoo.com/answer*
 // @match        https://*.answers.yahoo.com/question/*
 // @grant        none
 // ==/UserScript==
