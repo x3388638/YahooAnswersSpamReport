6f4cc8e2a17ec4f8ec5cbfe56066a262853e70ea
YY <z3388638@gmail.com>
2019-04-28 01:31:47 +0800

    Add metaBlock

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-// ==UserScript==
-// @name         YahooAnswersSpamReport
-// @namespace    https://2yc.tw
-// @version      0.2.1
-// @description  Make it easy to report a spam on Yahoo! Answers
-// @author       YY
-// @license      MIT
-// @homepage     https://github.com/x3388638/YahooAnswersSpamReport
-// @updateURL    https://openuserjs.org/meta/x3388638/YahooAnswersSpamReport.meta.js
-// @match        https://*.answers.yahoo.com
-// @match        https://*.answers.yahoo.com/answer*
-// @match        https://*.answers.yahoo.com/question/*
-// @match        https://*.answers.yahoo.com/dir/index*
-// @grant        none
-// ==/UserScript==
-
 (() => {
   const REPORTED_LIST = '@YahooAnswersSpanReport:reportedList';
   const MAX_REPORTED_RECORD = 50;
