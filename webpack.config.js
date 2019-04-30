2dcc1d17521e8004d3c15d57de1cccdd1e611e2c
YY <yuying.chang@verizonmedia.com>
2019-04-30 11:33:05 +0800

    Bundled by webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: __dirname,
+    filename: 'YahooAnswersSpamReport.user.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              'minify'
+            ],
+            comments: false
+          }
+        }
+      }
+    ]
+  }
+}
