1f82a0cdd49fff61e024ce16327b45617fa97afa
YY <z3388638@gmail.com>
2019-05-02 23:22:04 +0800

    v0.3.0

--- YahooAnswersSpamReport.user.js
+++ YahooAnswersSpamReport.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YahooAnswersSpamReport
 // @namespace    https://2yc.tw
-// @version      0.2.3
+// @version      0.3.0
 // @description  Make it easy to report a spam on Yahoo! Answers
 // @author       YY
 // @license      MIT
